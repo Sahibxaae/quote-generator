19921be0a0f55d9240a5eb0442c1f2fa21acb5c3
Sahibxaae <mohamedsahib.xaae@gmail.com>
2025-01-07 16:13:16 +0530

    changed the ui colors added props for initial quote when page loads

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ function App() {
   return (
     <div>
       <Header/>
-      <Main/>
+      <Main quote="I am not in danger, Skyler. I am the danger. A guy opens his door and gets shot, and you think that of me? No. I am the one who knocks!
+— Walter White"/>
     </div>
   );
 }
--- src/Main.js
+++ src/Main.js
@@ -1,5 +1,5 @@
 import React,{useState} from 'react';
-function Main (){
+function Main (props){
     const quotes = [
         "The greatest glory in living lies not in never falling, but in rising every time we fall. - Nelson Mandela",
         "The way to get started is to quit talking and begin doing. - Walt Disney",
@@ -8,9 +8,10 @@ function Main (){
         "In the middle of every difficulty lies opportunity. - Albert Einstein",
         "Happiness is only real when shared - Christopher Johnson McCandless",
         "Things you own ended up owning you -Tyler durden",
+        "I am not in danger, Skyler. I am the danger. A guy opens his door and gets shot, and you think that of me? No. I am the one who knocks! — Walter White"
       ];
     
-    const [currentQuote,setCurrentQuote] = useState(quotes[Math.floor(Math.random()*quotes.length)])
+    const [currentQuote,setCurrentQuote] = useState(props.quote)
     function genarateQuote(){
         const randomQuote = Math.floor(Math.random()*quotes.length);
         setCurrentQuote(quotes[randomQuote]);
@@ -19,8 +20,8 @@ function Main (){
     <div className='container'>
         <div className="display">
             "{currentQuote}"
-            <button className='next-btn' onClick={genarateQuote}>Generate</button>
         </div>
+        <button className='next-btn' onClick={genarateQuote}>Generate</button>
     </div>
   )
 }
